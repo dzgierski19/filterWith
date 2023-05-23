67f596dabecf49d81d48076142b66ea7a2bb54ae
dzgierski19 <dzgierskigit@gmail.com>
2023-05-24 02:51:15 +0300

    modified recursion

--- filterWith.ts
+++ filterWith.ts
@@ -1,34 +1,35 @@
 import { data, dataInterface } from "./data";
 
-const filterWith = <T>(obj: dataInterface[], phrase: T) => {
-  if (String(phrase).length <= 2) {
+const filterWith = (obj: dataInterface[], phrase: string | number) => {
+  const phraseToString = phrase.toString();
+  if (phraseToString.length <= 2) {
     return [];
   }
-  return obj.filter((value) => filterObject(value, phrase));
+  return obj.filter((value) => filterObject(value, phraseToString));
 };
 
-const filterObject = <T>(Obj: dataInterface, phrase: T): boolean => {
-  const phraseToString = String(phrase);
-  const phraseToRegExp = new RegExp(phraseToString);
+const filterObject = (Obj: dataInterface, phrase: string): boolean => {
   const objectValues = Object.values(Obj);
+  const phraseToRegExp = new RegExp(phrase);
   for (let i = 0; i < objectValues.length; i++) {
     if (phraseToRegExp.test(objectValues[i])) {
       return true;
     }
-    if (typeof objectValues[i] === "object") {
-      if (filterObject(objectValues[i], phrase)) {
-        return true;
-      }
+    if (
+      typeof objectValues[i] === "object" &&
+      filterObject(objectValues[i], phrase)
+    ) {
+      return true;
     }
   }
   return false;
 };
 
-const result1 = filterWith(data, "Jensen");
+const result1 = filterWith(data, "Sheppard");
 console.log(result1);
 
-// const result2 = filterWith(data, "nisi");
-// console.log(result2);
+const result2 = filterWith(data, "nisi");
+console.log(result2);
 
-// const result3 = filterWith(data, "Delacruz Acevedo");
-// console.log(result3);
+const result3 = filterWith(data, "Delacruz Acevedo");
+console.log(result3);
